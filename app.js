38796d3dd05c0cfaf5260087dcdf7fd94a0ba8bf
Martin Herrmann <martin.herrmann@visualistik.de>
2020-09-08 09:56:59 +0200

    use canvas timeline

--- app.js
+++ app.js
@@ -15,6 +15,17 @@ let app = (function () {
   let playerIntervalId;
   let currentImageIndex = 0;
 
+  const canvas = document.getElementById('timeline');
+  const rect = canvas.parentNode.getBoundingClientRect();
+  canvas.width = rect.width;
+  canvas.height = rect.height;
+
+  const ctx = canvas.getContext('2d');
+  const canvasHeight = canvas.offsetHeight;
+  const canvasWidth = canvas.offsetWidth;
+  const barWidth = canvasWidth/ (imageNames.length);
+  console.log('barWidth ', barWidth)
+  console.log('canvasWidth ', canvasWidth)
   //
   // Methods
   //
@@ -43,37 +54,33 @@ let app = (function () {
   };
 
 
-  const createTimeline = () => {
-    const timeLineElement = document.getElementById('timeline');
+  const drawTimeline = (mousePos = -50) => {
+    ctx.clearRect(0,0,canvasWidth, canvasHeight)
+    ctx.fillStyle = "#000";
     imageNames.forEach((imageName, i) => {
-        let height = 100;
-        if( i>1 ) {
-          const lastImageSeconds = parseInt(imageNames[i - 1].slice(11));
-          const currentImageSeconds = parseInt(imageName.slice(11));
-          height = Math.min(100, Math.abs(currentImageSeconds - lastImageSeconds) /10)
-        }
 
-        const outerDiv = document.createElement('div');
-        outerDiv.className = 'timeline__element';
-        outerDiv.id = 'timeline__element_' + i;
-        outerDiv.onclick = () => currentImageIndex = i;
-        outerDiv.style.width = (timeLineElement.offsetWidth / imageNames.length)  + 'px';
-
-        const div = document.createElement('div');
-        div.style.height = height + '%';
-        div.className = 'timeline__element-bar';
-        outerDiv.appendChild(div);
-        timeLineElement.appendChild(outerDiv);
+      if (i > 1) {
+        const lastImageSeconds = parseInt(imageNames[i - 1].slice(11));
+        const currentImageSeconds = parseInt(imageName.slice(11));
+        const height = Math.min(canvasHeight, Math.abs(currentImageSeconds - lastImageSeconds) / 10);
+        ctx.fillRect(barWidth*i, 0, barWidth, height);
+      }
     })
+
+    ctx.fillStyle = "#dae1eb";
+    ctx.fillRect(currentImageIndex * barWidth, 0, barWidth, canvasHeight);
+
+    ctx.fillStyle = "#018504";
+    ctx.fillRect(mousePos, 0, 5, canvasHeight);
   };
 
   publicAPIs.play = () => {
     playerIntervalId = setInterval(() => {
       addImageToCanvas(document.getElementById('imageCanvas'), imageNames[currentImageIndex]);
 
-      $('.timeline__element').css("background-color", "#333333");
       //document.getElementById('timeline__element_' + currentImageIndex).style.background = '#333333';
       currentImageIndex ++;
+      drawTimeline();
       document.getElementById('timeline__element_' + currentImageIndex).style.background = '#666';
 
       if(imageNames.length === currentImageIndex + 1) {
@@ -95,7 +102,14 @@ let app = (function () {
    * Another public method
    */
   publicAPIs.init = function () {
-    createTimeline();
+    drawTimeline();
+
+    $(canvas).on('mousemove',  (evt)=> {
+      const mousePosInElement = evt.pageX - $(canvas).offset().left;
+      console.log('mouseMove', mousePosInElement)
+      drawTimeline(mousePosInElement);
+    })
+
   };
 
 
